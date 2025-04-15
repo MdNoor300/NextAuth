6901d9f25a3a82dfd9d01ee5028ee747b9eaad48
MdNoor300 <sehzad.noor98@gmail.com>
2025-04-15 12:13:30 +0600

    making auth model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,3 +2,30 @@ import mongoose from 'mongoose';
 
 
 const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: [true,"please enter a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true,"please enter an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true,"please enter a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
